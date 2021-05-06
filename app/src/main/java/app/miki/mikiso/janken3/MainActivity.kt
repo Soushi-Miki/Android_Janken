d90deef1cf69d6b648299eea52a90de261033651
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-05-06 19:59:07 +0900

    じゃんけんアプリ3_04

--- app/src/main/java/app/miki/mikiso/janken3/MainActivity.kt
+++ app/src/main/java/app/miki/mikiso/janken3/MainActivity.kt
@@ -3,6 +3,7 @@ package app.miki.mikiso.janken3
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -16,17 +17,18 @@ class MainActivity : AppCompatActivity() {
 
             when (number) {
                 0 ->{
-                    cpu.text ="相手の手はグーです"
+                    //相手の手はグーです
+                    cpu.setImageResource(R.drawable.goo)
                     result.text ="引き分けです"
                     result.setTextColor(Color.parseColor("#FF64DD17"))
                 }
                 1 ->{
-                    cpu.text ="相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text="あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
                 }
                 2 ->{
-                    cpu.text="相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text="あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
                 }
@@ -39,17 +41,17 @@ class MainActivity : AppCompatActivity() {
 
             when (number){
                 0->{
-                    cpu.text="相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo)
                     result.text="あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
                 }
                 1->{
-                    cpu.text="相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text="引き分けです"
                     result.setTextColor(Color.parseColor("#FF64DD17"))
                 }
                 2->{
-                    cpu.text="相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text="あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
                 }
@@ -61,17 +63,17 @@ class MainActivity : AppCompatActivity() {
 
             when (number){
                 0->{
-                    cpu.text="相手の手はグーです"
+                    cpu.setImageResource(R.drawable.goo)
                     result.text="あなたの勝ちです"
                     result.setTextColor(Color.parseColor("#f44336"))
                 }
                 1->{
-                    cpu.text="相手の手はチョキです"
+                    cpu.setImageResource(R.drawable.choki)
                     result.text="あなたの負けです"
                     result.setTextColor(Color.parseColor("#2196f3"))
                 }
                 2->{
-                    cpu.text="相手の手はパーです"
+                    cpu.setImageResource(R.drawable.paa)
                     result.text="引き分けです"
                     result.setTextColor(Color.parseColor("#FF64DD17"))
                 }
